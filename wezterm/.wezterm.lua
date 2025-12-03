2e3786bb4eb48ee8bedbab57561fe12e9aaa59f8
Aman Nalakath <aman.nalakath@tum.de>
2025-12-03 12:55:03 +0100

    wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.initial_cols = 120
+config.initial_rows = 28
+
+config.font_size = 10
+
+config.color_scheme = 'AtelierSulphurpool'
+
+config.window_background_opacity = 0.95
+
+config.enable_scroll_bar = true
+
+config.enable_tab_bar = true
+
+config.use_fancy_tab_bar = true
+
+config.window_close_confirmation = 'AlwaysPrompt'
+
+config.window_frame = {
+  font = require('wezterm').font 'Jet Brains Mono',
+  font_size = 12,
+}
+
+return config
